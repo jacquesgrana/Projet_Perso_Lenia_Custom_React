7f44f854e1700bae5529e73c7c87fd998552e76d
jacques <jac.granarolo.info@gmail.com>
2024-05-08 15:44:18 +0200

    deploiement sur github.io

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Webapp codée en Typescript sous React, simulation mathématique inspirée de Lenia :
 
 [vidéo youtube](https://www.youtube.com/watch?v=PlzV4aJ7iMI)
+
+[site sur github.io](https://jacquesgrana.github.io/Projet_Perso_Lenia_Custom_React/)
--- src/config/CanvasConfig.ts
+++ src/config/CanvasConfig.ts
@@ -1,5 +1,5 @@
 export default class CanvasConfig {
-    public static readonly CANVAS_WIDTH = 600;
-    public static readonly CANVAS_HEIGHT = 600;
-    public static readonly CANVAS_DELAY = 200;
+    public static readonly CANVAS_WIDTH = 1200;
+    public static readonly CANVAS_HEIGHT = 700;
+    public static readonly CANVAS_DELAY = 0;
 }
